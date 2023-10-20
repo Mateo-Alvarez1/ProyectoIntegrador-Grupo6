51f0446943d9290c73cc1ecfbcd88ef75f406496
MatiasNicolasVazquez <mnvm2509@gmail.com>
2023-10-19 21:57:42 -0300

    Estilado de errores de form y background en light

---
+++ src/frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import { header } from "request/lib/hawk"
+import logo from "../../assets/logoPitchPlease.png"
+
+
+
+const Header = () => {
+  return (
+    <>
+    <header className="headerContainer">
+        <div className="logoContainer">
+
+       
+        <img src={logo} alt="" className="logo" />
+
+        <h2 className="logoName">PitchPlease</h2>
+
+        </div>
+
+      <div className="menuContainer">
+    <img src="" alt="menu hamburguesa" className="burgerMenu" />
+
+    <a className="registerButton">Iniciar Sesión</a>
+
+    <a className="registerButton">Crear Cuenta</a>
+    </div>
+
+    </header>
+
+  
+    
+
+    </>
+  )
+}
+
+export default Header
