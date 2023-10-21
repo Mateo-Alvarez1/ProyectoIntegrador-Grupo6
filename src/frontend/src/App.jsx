fb83acedb5a4d28d41bf70599afadbeb9bfdbe36
Anni <annipcrt@gmail.com>
2023-10-21 19:49:10 -0300

    initial branch commit

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,6 +3,8 @@ import './Components/Header/header.css'
 import Header from './Components/Header/Header'
 import { Categorias } from './Components/Categorias/Categorias'
 import { Buscador } from './Components/Buscador/Buscador'
+import { Route, Routes } from 'react-router-dom'
+import Admin from './pages/Admin/Admin'
 
 function App() {
 
@@ -11,6 +13,10 @@ function App() {
       <Header/>
       {/* <Buscador/> */}
       <Categorias/>
+      <Routes>
+        { /* <Route path="/" element={} /> */ }
+        <Route path="/admin" element={<Admin/>} />
+      </Routes>
     </>
   )
 }
