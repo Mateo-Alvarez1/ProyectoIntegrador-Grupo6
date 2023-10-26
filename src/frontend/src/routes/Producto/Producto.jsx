7dd03dbcba2f908f176140d005d13e741e0d808d
MatiasNicolasVazquez <mnvm2509@gmail.com>
2023-10-26 19:35:49 -0300

    Correciones detalle de producto y admin

--- src/frontend/src/routes/Producto/Producto.jsx
+++ src/frontend/src/routes/Producto/Producto.jsx
@@ -1,44 +1,70 @@
-import React from 'react'
-import {Link, useParams } from 'react-router-dom'
-import arrow from "../../assets/arrow.svg"
-import "./Producto.css"
-import Guitarra from "../../assets/GuitarraClasica.jpg"
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import arrow from "../../assets/arrow.svg";
+import "./Producto.css";
+import Productos from "../../utils/products.json";
 
 const Producto = () => {
+  const [producto, setProducto] = useState(null);
+  const { productoId } = useParams();
 
-const {productoId} = useParams();
-
-
+  useEffect(() => {
+    const foundProduct = Productos.find((product) => product.id == productoId);
+    if (foundProduct) {
+      setProducto(foundProduct);
+    }
+  }, [productoId]);
 
   return (
     <>
-    <div className='subHeader'>
-        <h2>{productoId}</h2>
-        <Link to="/" className='textArrow'>Volver al Inicio <img src={arrow} alt="" /></Link>
-    </div>
-    <div className='imageContainer'>
-
+      <div className='subHeader'>
+        {producto && <h2>{producto.nombre}</h2>}
+        <Link to="/" className='textArrow'>
+          Volver al Inicio <img src={arrow} alt="" />
+        </Link>
+      </div>
+      <div className='imageContainer'>
         <div>
-        <div className='containerImagenPrincipal'> <img src={Guitarra}  alt="" /></div>
+          <div className='containerImagenPrincipal'>
+            {producto && <img src={producto.imagen} alt={producto.nombre} />}
+          </div>
         </div>
-       <div className='containerSecundarias'>
-        <div className="containerSecundarias1">
-        <div className='imagen2'> <img src={Guitarra}  alt="" /></div>
-        <div className='imagen3'> <img src={Guitarra}  alt="" /></div>
-
+        <div className='containerSecundarias'>
+          <div className="containerSecundarias1">
+            <div className='imagen2'>
+              {producto && <img src={producto.imagen} alt={producto.nombre} />}
+            </div>
+            <div className='imagen3'>
+              {producto && <img src={producto.imagen} alt={producto.nombre} />}
+            </div>
+          </div>
+          <div className='containerSecundarias2'>
+            <div className='imagen4'>
+              {producto && <img src={producto.imagen} alt={producto.nombre} />}
+            </div>
+            <div className='imagen5'>
+              {producto && <img src={producto.imagen} alt={producto.nombre} />}
+            </div>
+          </div>
         </div>
-
-        <div className='containerSecundarias2'>
-        <div className='imagen4'> <img src={Guitarra}  alt="" /></div>
-        <div className='imagen5'> <img src={Guitarra}  alt="" /></div>
+        <div>
+          
         </div>
-
-       </div>
-  
+      </div>
+      <div className='specsProducto'>
+      {producto && (
+            <>
+                  <h2>Especificaciones técnicas</h2>
+                 
+              <p>Categoría: {producto.categoria}</p>
+              <p>Precio: USD {producto.precio}</p>
+              <p>Marca: {producto.marca}</p>
+            </>
+          )}
+          </div>
       
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Producto
+export default Producto;
