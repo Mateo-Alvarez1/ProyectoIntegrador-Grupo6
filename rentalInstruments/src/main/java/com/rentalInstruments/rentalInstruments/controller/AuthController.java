c6f33cba75cb440f2b3b3ebdd57086a9d5433127
Mateo-Alvarez1 <mateoalvarez384@gmail.com>
2023-10-31 21:32:32 -0300

    Jwt authorization & authentication

---
+++ rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.rentalInstruments.rentalInstruments.controller;
+
+import com.rentalInstruments.rentalInstruments.model.AuthenticationRequest;
+import com.rentalInstruments.rentalInstruments.model.AuthenticationResponse;
+import com.rentalInstruments.rentalInstruments.model.RegisterRequest;
+import com.rentalInstruments.rentalInstruments.exceptions.ObjectAlreadyExists;
+import com.rentalInstruments.rentalInstruments.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/registrar")
+    public ResponseEntity<String> registrarUsuario(@RequestBody RegisterRequest registerRequest) throws ObjectAlreadyExists {
+        return ResponseEntity.ok(authenticationService.registrarUsuario(registerRequest));
+    }
+    @PostMapping("/autenticar")
+    public ResponseEntity<AuthenticationResponse> AutenticarUsuario(@RequestBody AuthenticationRequest authenticationRequest) throws ObjectAlreadyExists {
+        return ResponseEntity.ok(authenticationService.autenticarUsuario(authenticationRequest));
+    }
+
+}
