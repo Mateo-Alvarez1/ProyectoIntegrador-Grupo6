bc1401a778146a0b1bbb10c45eef2d78ac54e951
MatiasNicolasVazquez <mnvm2509@gmail.com>
2023-11-04 17:16:04 -0300

    correcion puerto cors

--- rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/configuration/CorsConfig.java
+++ rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/api/v1/**") // Aquí ajustamos la ruta
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins("http://localhost:5173")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowCredentials(true);
         }
