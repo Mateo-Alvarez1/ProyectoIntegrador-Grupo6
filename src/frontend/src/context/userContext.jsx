daf10bae45449f8e5cf3a6b2f0a4e1f8d6b24041
annipcrt <annipcrt@gmail.com>
2023-11-07 20:04:45 -0300

    login pronto (creo)

--- src/frontend/src/context/userContext.jsx
+++ src/frontend/src/context/userContext.jsx
@@ -6,19 +6,38 @@ const Provider = userContext.Provider
 const UserContextProvider = ({children}) => {
 
     // acá se inicializa el estado del usuario con el jwt 
-    const [userJwt, setUserJwt] = useState(null) // en el null va el jwt traido del localstorage
-    const [user, setUser] = useState(null) // en el null va el usuario traido del localstorage (parseado)
+    const [userJwt, setUserJwt] = useState(localStorage.getItem('jwt')) // en el null va el jwt traido del localstorage
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user'))) // en el null va el usuario traido del localstorage (parseado)
     
 
     const login = (user) => {
-        // acá se guarda el jwt en el localstorage
+        // acá se setea y se guarda el jwt en el localstorage
+        setUserJwt(user.jwt)
+        localStorage.setItem('jwt', JSON.stringify(user.jwt))
+
         // y se setea el estado del usuario
+        setUser(user)
+        localStorage.setItem('user', JSON.stringify(
+        {
+          id: user.id,
+          name: user.name,
+          lastname: user.lastname,
+          email: user.email,
+          role: user.role,
+        }
+        ))
         // el fetch de login se hace en el componente Login.jsx
     }
 
     const logout = () => {
         // acá se borra el jwt del localstorage
+        setUserJwt(null)
+        localStorage.removeItem('jwt')
+
         // y se setea el estado del usuario
+        setUser(null)
+        localStorage.removeItem('user')
+
     }
 
   return (
--- src/frontend/src/routes/Login/Login.jsx
+++ src/frontend/src/routes/Login/Login.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Input from "../../Components/Input/Input";
 import "./Login.css"
-//import { useNavigate } from "react-router";
+import { useNavigate } from "react-router";
+import { userContext } from "../../context/userContext";
 //import { setUserContext } from "../../context/userContext"; 
 
 const Login = () => {
   // estados para los campos
   const [userEmail, setUserEmail] = useState({ value: '', valid: null });
   const [userPassword, setUserPassword] = useState({ value: '', valid: null });
-
-  //const [error, setError] = useState('');
+  
+  const [isFormValid, setIsFormValid] = useState(null);
+  const [error, setError] = useState('');
     
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { login } = useContext(userContext);
+
+  const regex = {
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, // mínimo 8 caracteres, al menos una letra y un número
+  };
+
 
   // const setUser = (user) => {
   //   setUserContext(user);
@@ -31,26 +40,38 @@ const Login = () => {
     });
   };
 
-  const login = async (email, password) => {
-    
-    //setUser({email, password})
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     validateInputs();
+    setIsFormValid(true);
     
 
-    if (userEmail && userPassword) {
+    if (userEmail.valid === 'true' && userPassword.valid === 'true') {
+      const data = {
+        email: userEmail.value,
+        password: userPassword.value,
+      }
       try {
-        await login(userEmail, userPassword);
-        //logueo exitoso
-        //navigate("/");
+        const response = await fetch(`http://localhost:8080/api/v1/auth/autenticar`, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        })
+
+        if (response.ok) {
+          const user = await response.json();
+          login(user);
+          navigate('/home');
+        }
     }
       catch (error) {
-    // ha ocurrido un error al loguear al usuario
-    // mostrar un mensaje de error al usuario
-    // ...
+        setIsFormValid(false);
+        setError(
+          'No se ha podido iniciar sesión. Por favor, vuelva a intentarlo.'
+        );
   }
   }
 }
@@ -68,6 +89,7 @@ const Login = () => {
               name='userEmail'
               error='No se encuentra usuario registrado con este email.'
               placeholder='Ingrese su email'
+              regex={regex.email}
               
             />
       <Input className= "input"
@@ -79,8 +101,13 @@ const Login = () => {
               name='userPassword'
               error='La contraseña no es correcta.'
               placeholder='Ingrese su contraseña'
-              
+              regex={regex.password}
             />
+        {isFormValid === false && (
+          <p className="error">
+            {error ? error : "Los datos son inválidos, por favor vuelva a intentarlo."}
+          </p>
+        )}
       <button className= "button" type="submit">Iniciar sesión</button>
       </form>
     </div>
