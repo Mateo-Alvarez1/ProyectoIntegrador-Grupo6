b5f17a38d5a1239a7d1e4bbeb0dce8142cb65c65
Cecilia <cecilia.audap@gmail.com>
2023-11-20 10:54:53 -0300

    nn

--- rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/Repository/CategoriaRepository.java
+++ rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/Repository/CategoriaRepository.java
@@ -15,7 +15,7 @@ public interface CategoriaRepository extends JpaRepository<Categoria , Long> {
     @Query("SELECT c FROM Categoria c WHERE c.nombre = ?1")
     Optional<Categoria> findByNombre(String nombre);
 
-    void delete(Long id);
+
 
 
 }
