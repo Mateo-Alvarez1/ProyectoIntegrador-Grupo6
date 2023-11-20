ea616285ea3592f885540969c810f2465a8309e4
Cecilia <cecilia.audap@gmail.com>
2023-11-20 19:22:01 -0300

    nn

--- rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/exceptions/GlobalExceptionHandler.java
+++ rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.rentalInstruments.rentalInstruments.exceptions;
 
-import com.amazonaws.services.connect.model.UserNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
