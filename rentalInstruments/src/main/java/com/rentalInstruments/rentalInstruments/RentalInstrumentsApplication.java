5d0c43e24beaefb7a7c3e1cd71d527a7850754d0
Cecilia <cecilia.audap@gmail.com>
2023-11-28 17:28:50 -0300

    nn

--- rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/RentalInstrumentsApplication.java
+++ rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/RentalInstrumentsApplication.java
@@ -2,8 +2,10 @@ package com.rentalInstruments.rentalInstruments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "RentalInstrumentsApplication")
 public class RentalInstrumentsApplication {
 
 	public static void main(String[] args) {
