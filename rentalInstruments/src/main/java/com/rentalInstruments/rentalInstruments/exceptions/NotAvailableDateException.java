e5b292894fafea3c4c99bee584b5a0042ec0e137
MatiasNicolasVazquez <mnvm2509@gmail.com>
2023-12-07 00:32:14 -0300

    validacion de fechas reservas

---
+++ rentalInstruments/src/main/java/com/rentalInstruments/rentalInstruments/exceptions/NotAvailableDateException.java
@@ -0,0 +1,5 @@
+package com.rentalInstruments.rentalInstruments.exceptions;
+
+public class NotAvailableDateException extends Exception {
+    public NotAvailableDateException(String message){super (message);}
+}
